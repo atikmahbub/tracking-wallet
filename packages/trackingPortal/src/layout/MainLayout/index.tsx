9ca6ca6bc1743407b0c83ac8a99f11a52720bd04
atikmahbub <atikmahbub100@gmail.com>
2024-10-04 02:05:35 +0600

    configured main layout

--- packages/trackingPortal/src/layout/MainLayout/index.tsx
+++ packages/trackingPortal/src/layout/MainLayout/index.tsx
@@ -1,13 +1,29 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { Box, Grid2 } from "@mui/material";
 import Loader from "@trackingPortal/components/Loader";
 import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 const MainLayout: React.FC = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <Outlet />
-    </Suspense>
+    <Box component="main" display="flex" justifyContent="center">
+      <Grid2
+        container
+        maxWidth="xl"
+        width="100%"
+        sx={{
+          px: { xs: 0, sm: 2 },
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          position: "relative",
+        }}
+      >
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </Grid2>
+    </Box>
   );
 };
 
