cff0cfb8c5fbd0737641eba568a3ab9d04cf7506
atikmahbub <atikmahbub100@gmail.com>
2024-10-03 14:07:30 +0600

    added fe portal as workspace

---
+++ packages/trackingPortal/Readme.md
@@ -0,0 +1,58 @@
+# React Typescript Webpack boilerplate
+
+This is a React TypeScript application built with Webpack.
+
+## Getting Started
+
+Follow these steps to set up and run the project on your local machine.
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+- [Node.js](https://nodejs.org/) (which includes npm)
+- [Yarn](https://yarnpkg.com/)
+
+### Installation and Setup
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/atikmahbub/react-typescript-webpack-boilerplate
+   ```
+
+2. **Navigate into the project directory:**
+
+   ```bash
+   cd react-typescript-webpack-boilerplate
+   ```
+
+3. **Install dependencies:**
+
+   ```bash
+   yarn install
+   ```
+
+### Running the Application
+
+To start the development server:
+
+```bash
+yarn start
+```
+
+This will start the application at `http://localhost:8080` by default.
+
+### Building the Application
+
+To create a production build:
+
+```bash
+yarn build
+```
+
+This command will bundle the application and output the production files into the `dist` directory.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ packages/trackingPortal/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const configFilePath = require.resolve("./tsconfig.json");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin({ configFile: configFilePath })],
+    alias: {
+      "@trackingPortal": path.resolve(__dirname, "src"),
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        use: "json-loader",
+      },
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules|\.d\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            projectReferences: true,
+            configFile: configFilePath,
+            compilerOptions: {
+              noUnusedLocals: false,
+              noUnusedParameters: false,
+            },
+          },
+        },
+      },
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, "node_modules")],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.ttf$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+};
